24c988ee1418efcaf5544fd3a9eb126225aaae94
Unknown <barudy.diego@gmail.com>
2017-12-20 09:52:22 +0100

    First commit

---
+++ Assets/CameraShot.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CameraShot : EditorWindow
+{
+    #region Private and Protected Members
+
+    #endregion
+    #region Public Members
+
+    #endregion
+    public Transform editorCam;
+
+    #region Public void
+
+    #endregion
+
+    #region System
+    [MenuItem("Toolbox/CamShot")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(CameraShot));
+    }
+    private void OnGUI()
+    {
+        editorCam = GetEditorCamera(SceneView.GetAllSceneCameras());
+
+        if (GUILayout.Button("Shot"))
+        {
+            DoShot();
+        }
+    }
+    private void OnFocus()
+    {
+        
+    }
+    #endregion
+
+    #region Tools Debug and Utility
+    public Transform DoShot()
+    {
+        return editorCam;
+    }
+    private Transform GetEditorCamera(Camera[] _cams)
+    {
+        
+        foreach (Camera c in _cams)
+        {
+            if (c.cameraType == CameraType.SceneView)
+            {
+                return c.transform;
+            }
+        }
+
+        return null;
+    }
+    #endregion
+
+
+}
